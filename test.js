a908a2e40e5ade8698b87453a9f2f0b678bd7549
morganherlocker <morgan.herlocker@gmail.com>
2015-02-05 18:23:54 -0500

    refactor tests

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
 var test = require('tape');
 var center = require('./');
-var fc = require('./geojson/fc');
+var fs = require('fs');
+
+var boxFC = JSON.parse(fs.readFileSync(__dirname+'/fixtures/in/box.geojson'));
+var blockFC = JSON.parse(fs.readFileSync(__dirname+'/fixtures/in/block.geojson'));
 
 test('center', function(t){
-  var centered = center(fc);
-  centered.properties['marker-color'] = '#f0f'
-  t.ok(centered, 'should return the proper center for a FeatureCollection');
-  t.deepEqual(centered.geometry.coordinates, [75, -3]);
-  fc.features.push(centered)
-  console.log(JSON.stringify(fc))
+  var boxFcCenter = center(boxFC);
+  boxFcCenter.properties['marker-color'] = '#f0f';
+  t.ok(boxFcCenter, 'should return the proper center for a FeatureCollection');
+  t.deepEqual(boxFcCenter.geometry.coordinates, [65.56640625, 43.59448261855401]);
+
+  var blockFcCenter = center(blockFC.features[0]);
+  blockFcCenter.properties['marker-color'] = '#f0f';
+  t.ok(blockFcCenter, 'should return the proper center for a FeatureCollection');
+  t.deepEqual(blockFcCenter.geometry.coordinates, [ -114.02911397119072, 51.050271120392566]);
+
+  boxFC.features.push(boxFcCenter);
+  blockFC.features.push(blockFcCenter);
+  fs.writeFileSync(__dirname+'/fixtures/out/box_out.geojson', JSON.stringify(boxFC,null,2));
+  fs.writeFileSync(__dirname+'/fixtures/out/block_out.geojson', JSON.stringify(blockFC,null,2));
+
   t.end();
 });
