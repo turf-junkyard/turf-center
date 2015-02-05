69fad8ebf20700f199d475e6220b6a8f867f6157
morganherlocker <morgan.herlocker@gmail.com>
2015-02-05 18:15:04 -0500

    add centroid fixtures

--- test.js
+++ test.js
@@ -4,7 +4,10 @@ var fc = require('./geojson/fc');
 
 test('center', function(t){
   var centered = center(fc);
+  centered.properties['marker-color'] = '#f0f'
   t.ok(centered, 'should return the proper center for a FeatureCollection');
   t.deepEqual(centered.geometry.coordinates, [75, -3]);
+  fc.features.push(centered)
+  console.log(JSON.stringify(fc))
   t.end();
 });
